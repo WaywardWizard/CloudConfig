eaacd0af4dd4e41f686ca88f42abe116267b934b
Ben Tomlin <obscured@>
2021-01-02 11:20:30 +1300

    compression tester

--- scripts/bin/testCompression.bash
+++ scripts/bin/testCompression.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
-pixz < $1 > $1.pixz
-pbzip2 -k $1
-lbzip2 -k $1
-zstd - < $1 > $1.zst
+if hash pixz;then
+	echo pixz
+	pv -ptr $1 | pixz > $1.pixz
+fi
+if hash pbzip2;then
+	echo pbzip2
+	pv -ptr $1 | pbzip2 -c > $1.pbzip2
+fi
+if hash lbzip2;then
+	echo lbzip2
+	pv -ptr $1 | lbzip2 -c > $1.lbzip2
+fi
+if hash zstd;then
+	echo zstd
+	pv -ptr $1 | zstd - > $1.zst
+fi
